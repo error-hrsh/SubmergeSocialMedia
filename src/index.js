8b9b0f368112264e66c35164f9f8e1bfceff3512
Hrsh <hrshradadiya597@gmail.com>
2026-09-09 09:17:20 +0530

    utils learn and publish

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import dotenv from "dotenv";
 dotenv.config();
 // import "dotenv/config";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(5000, () => {
+      console.log(`App is running on the port number 5000`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Error in db`);
+  });
