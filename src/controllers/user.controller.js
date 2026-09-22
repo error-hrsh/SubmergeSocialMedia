75565c642ff0276024cd761ec6c6c986459cd2a4
Hrsh <hrshradadiya597@gmail.com>
2026-09-22 08:18:15 +0530

    MONGO DB Aggregation Added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -174,7 +174,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
       incomingRefreshToken,
       process.env.ACCESS_TOKEN_SECRET
     );
-    const user = User.findById(decodedToken?._id);
+    const user = await User.findById(decodedToken?._id);
     if (!user) {
       throw new ApiError(401, "Invalid refresh Token");
     }
@@ -221,13 +221,15 @@ const chnageCurrentPassword = asyncHandler(async (req, res) => {
 });
 
 const getCurentUser = asyncHandler(async (req, res) => {
-  return res.status(200).json(200, req.body, "Curent User fetched Succesfully");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, req.body, "Curent User fetched Succesfully"));
 });
 const updateAccountDetails = asyncHandler(async (req, res) => {
   const { fullName, email } = req.body;
   if (!(fullName || email)) {
     throw new ApiError(400, "All Feilds are Reqwire");
-    (User.findByIdAndUpdate(req.user?._id, {
+    (await User.findByIdAndUpdate(req.user?._id, {
       $set: {
         fullName: fullName,
         email: email,
@@ -300,7 +302,76 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, user, "Cover image updated successfully"));
 });
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
 
+  if (!username?.trim()) {
+    throw new ApiError(400, "username is missing");
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers",
+        },
+        channelsSubscribedToCount: {
+          $size: "$subscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+      },
+    },
+  ]);
+
+  if (!channel?.length) {
+    throw new ApiError(404, "channel does not exists");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, channel[0], "User channel fetched successfully")
+    );
+});
 ///////Final///////////////
 //ends
 export {
